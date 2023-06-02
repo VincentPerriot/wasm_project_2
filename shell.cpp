bce7cb24f5c3f40a7a70abf537bdbb8739890bd0
Vincent Perriot <32515747+VincentPerriot@users.noreply.github.com>
2023-06-02 16:11:08 +0200

    "inititial commit"

---
+++ shell.cpp
@@ -0,0 +1,12 @@
+﻿// wasm_project_2.cpp : Defines the entry point for the application.
+//
+
+#include "wasm_project_2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

fa8071530b03bd9927cb2e84d7d1feeee4b67176
Vincent Perriot <32515747+VincentPerriot@users.noreply.github.com>
2023-10-08 16:15:42 +0200

    "Back"

--- phys.h
+++ phys.h
@@ -9,86 +9,83 @@
 #include "imgui_impl_opengl3.h"
 #include "maths.h"
 
-class Helix
+class Particle
 {
+};
+
+
+class Simulation {
 public:
-
-	Helix() = default;
-
-	Helix(Mesh a_mesh, int a_resolution, vec3 a_localUp, std::vector<float> a_colors)
+	Simulation() 
 	{
-
-		colors = a_colors;
-
-		mesh = a_mesh;
-		resolution = a_resolution;
-		localUp = a_localUp;
-
-		axisA[0] = localUp.y();
-		axisA[1] = localUp.z();
-		axisA[2] = localUp.x();
-		axisB = cross(localUp, axisA);
-
-		constructMesh();
 	}
 
-	void update()
+	Simulation() = default;
+
+	void DrawCircle(GLuint programID)
 	{
-		constructMesh();
 	}
 
-	void constructMesh()
+	void setBaseGUI(GLFWwindow* window)
 	{
-		std::vector<Vertex> vertices(resolution * resolution);
-		std::vector<GLuint> triangles((resolution - 1) * (resolution - 1) * 6);
-		int triIndex = 0;
+		// Setup Dear ImGui context
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; 
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
 
-		for (int y = 0; y < resolution; y++)
+		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+		ImGuiStyle& style = ImGui::GetStyle();
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
-			for (int x = 0; x < resolution; x++)
-			{
-				int i = x + y * resolution;
-				vec2 percent = vec2((double)x / (resolution - 1), (double)y / (resolution - 1));
-				vec3 pointOnUnitCylinder = localUp + (percent.x() - 0.5) * 2.0 * axisA + (percent.y() - 0.5) * 2.0 * axisB;
-
-				vertices[i].Pos[0] = (float)pointOnUnitCylinder.x();
-				vertices[i].Pos[1] = (float)pointOnUnitCylinder.y();
-				vertices[i].Pos[2] = (float)pointOnUnitCylinder.z();
-
-				vertices[i].Colors[0] = colors[0];
-				vertices[i].Colors[1] = colors[1];
-				vertices[i].Colors[2] = colors[2];
-
-				vertices[i].Normal[0] = (float)pointOnUnitCylinder.x();
-				vertices[i].Normal[1] = (float)pointOnUnitCylinder.y();
-				vertices[i].Normal[2] = (float)pointOnUnitCylinder.z();
-				
-				// Draw counter Clockwise
-				if (x != (resolution - 1) && y != (resolution - 1))
-				{
-					triangles[triIndex] = i;
-					triangles[triIndex + 1] = i + resolution;
-					triangles[triIndex + 2] = i + resolution + 1;
-
-					triangles[triIndex + 3] = i;
-					triangles[triIndex + 4] = i + resolution + 1;
-					triangles[triIndex + 5] = i + 1;
-					triIndex += 6;
-				}
-			}
+			style.WindowRounding = 0.2f;
+			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
 		}
 
+		// Setup Platform/Renderer backends
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init("#version 300 es");
 
-		mesh = Mesh(vertices, triangles);
 	}
 
-	~Helix() {}
+	void update(GLuint programId)
+	{
+		DrawCircle(programId);
+	}
+
+	void RenderUI(GLFWwindow* window)
+	{
+		glfwSwapInterval(1); // Enable vsync
+
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Fluid Simulation!");                          // Create a window
+
+		ImGui::Text("Change settings to observe real time changes");
+
+		ImGui::End();
+
+		// Rendering
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	~Simulation() {};
 
 public:
-	Mesh mesh;
-	int resolution;
-	vec3 localUp;
-	vec3 axisA;
-	vec3 axisB;
-	std::vector<float> colors;
+
+private:
+	ImGuiIO io;
 };
+
+
